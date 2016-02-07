9b594119c6cf1dbf7e9235382861883116318f8c
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-02-07 12:48:48 -0500

    fixes

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ var async = require('async')
 
 module.exports = collectPhoneNumbers = function() {
   var regex = /((\(\d{3}\)|\d{3})(-|\.|\s)\d{3}(-|\.|\s)\d{4}|\d{10})/g
-  var inputDirectory
-  if (process.argv[2]) {
-    inputDirectory = __dirname + "/" + process.argv[2]
-  } else {
-    inputDirectory = "."
-  }
-  var outputFile = process.argv[3] || "phone-numbers.txt"
+  
+  var inputDirectory = process.argv[2]
+  var outputFile = process.argv[3]
 
   fs.readdir(inputDirectory, function (err, list) {
     var numbers = []
     async.forEach(list, function (file, callback) {
-      console.log(file)
-      fs.readFile(__dirname + "/" + file, 'utf8', function (err, data) {
+      fs.readFile(inputDirectory + "/" + file, 'utf8', function (err, data) {
         if (data) {
           var matches = data.match(regex)
           if (matches) {
