e0e129cb9d892c9b1a6d11dd02aa43822947b59a
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-02-07 12:17:38 -0500

    initial commit, project more or less works

---
+++ index.js
@@ -0,0 +1,42 @@
+require('locus')
+var fs = require('fs')
+var async = require('async')
+
+
+module.exports = collectPhoneNumbers = function() {
+  var regex = /((\(\d{3}\)|\d{3})(-|\.|\s)\d{3}(-|\.|\s)\d{4}|\d{10})/g
+  var inputDirectory
+  if (process.argv[2]) {
+    inputDirectory = __dirname + "/" + process.argv[2]
+  } else {
+    inputDirectory = "."
+  }
+  var outputFile = process.argv[3] || "phone-numbers.txt"
+
+  fs.readdir(inputDirectory, function (err, list) {
+    var numbers = []
+    async.forEach(list, function (file, callback) {
+      console.log(file)
+      fs.readFile(__dirname + "/" + file, 'utf8', function (err, data) {
+        if (data) {
+          var matches = data.match(regex)
+          if (matches) {
+            eval(locus)
+            console.log("Phone numbers found in " + file + ": " + matches)
+            numbers = numbers.concat(matches)
+          }
+        }
+        callback()
+      })
+    }, function(err) {
+      if (numbers.length === 0) {
+        console.log("No phone numbers found.")
+      } else {
+        fs.writeFile(outputFile, numbers.join(",\n"), function(err) {
+          if (err) throw err;
+          console.log("Saved phone numbers in " + outputFile)
+        })
+      }
+    })
+  })
+}
